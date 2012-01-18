e7bf46e55eec522aeaf9e0a5d429b09567a9a55f
Gergely Zombori <zomboir@gmail.com>
2012-01-18 14:12:08 +0000

    #1244 - Finished off OpenIGTLink Message types --> need to update version tag
    git-svn-id: http://cmicdev.cs.ucl.ac.uk/svn/cmic/trunk/NiftyLink@8261 30ced1d1-d8fb-0310-b896-84269ee58dd6

--- CMake/CMakeExternals/OPENIGTLINK.cmake
+++ CMake/CMakeExternals/OPENIGTLINK.cmake
@@ -34,7 +34,7 @@ SET(OPENIGTLINK_DEPENDS ${proj})
  
 IF(NOT DEFINED OpenIGTLink_DIR)
 
-    SET(revision_tag 63a697e7bd)
+    SET(revision_tag 033e851c02)
     IF(${proj}_REVISION_TAG)
         SET(revision_tag ${${proj}_REVISION_TAG})
     ENDIF()
