84fdaf499dbcb8bb924bb14e747ebce2d3c8bee3
Gergely Zombori <zomboir@gmail.com>
2012-01-20 15:09:11 +0000

    #1253 - OpenIGTLink code updated, changing revision tag
    git-svn-id: http://cmicdev.cs.ucl.ac.uk/svn/cmic/trunk/NiftyLink@8289 30ced1d1-d8fb-0310-b896-84269ee58dd6

--- CMake/CMakeExternals/OPENIGTLINK.cmake
+++ CMake/CMakeExternals/OPENIGTLINK.cmake
@@ -34,7 +34,7 @@ SET(OPENIGTLINK_DEPENDS ${proj})
  
 IF(NOT DEFINED OpenIGTLink_DIR)
 
-    SET(revision_tag 15bda37031)
+    SET(revision_tag 6b82eb37cb)
     IF(${proj}_REVISION_TAG)
         SET(revision_tag ${${proj}_REVISION_TAG})
     ENDIF()
