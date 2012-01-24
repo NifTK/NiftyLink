a1698f981badab1dcb8cb31164617c1f716850ea
Gergely Zombori <zomboir@gmail.com>
2012-01-24 10:46:52 +0000

    #1269 - Update cmake file to use latest (hopefully bug-fixed) OpenIGTLink
    git-svn-id: http://cmicdev.cs.ucl.ac.uk/svn/cmic/trunk/NiftyLink@8313 30ced1d1-d8fb-0310-b896-84269ee58dd6

--- CMake/CMakeExternals/OPENIGTLINK.cmake
+++ CMake/CMakeExternals/OPENIGTLINK.cmake
@@ -34,7 +34,7 @@ SET(OPENIGTLINK_DEPENDS ${proj})
  
 IF(NOT DEFINED OpenIGTLink_DIR)
 
-    SET(revision_tag 2b9a418b66)
+    SET(revision_tag 85f359f008)
     IF(${proj}_REVISION_TAG)
         SET(revision_tag ${${proj}_REVISION_TAG})
     ENDIF()
