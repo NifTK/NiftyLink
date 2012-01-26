9687a6345446da8a9b39644c5fb60b69f0e7fe1c
Matt Clarkson <m.clarkson@ucl.ac.uk>
2012-01-26 09:25:38 +0000

    #1279 - update OpenIGTLink to 68b46acdf6
    git-svn-id: http://cmicdev.cs.ucl.ac.uk/svn/cmic/trunk/NiftyLink@8350 30ced1d1-d8fb-0310-b896-84269ee58dd6

--- CMake/CMakeExternals/OPENIGTLINK.cmake
+++ CMake/CMakeExternals/OPENIGTLINK.cmake
@@ -35,7 +35,7 @@ SET(OPENIGTLINK_DEPENDS ${proj})
 IF(NOT DEFINED OpenIGTLink_DIR)
 
     #1273 - OpenIGTLink version update due to bugfixes.
-    SET(revision_tag 9beb7581f6)
+    SET(revision_tag 68b46acdf6)
     IF(${proj}_REVISION_TAG)
         SET(revision_tag ${${proj}_REVISION_TAG})
     ENDIF()
