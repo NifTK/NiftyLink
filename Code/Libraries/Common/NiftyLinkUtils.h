1de6c8fa46e5e120dd0d697b7871cac1cecaaa69
Gergely Zombori <zomboir@gmail.com>
2012-02-13 12:50:57 +0000

    #1303 - Defined all OpenIGTLink message types in an Enum, which enables using switch - case on them
    git-svn-id: http://cmicdev.cs.ucl.ac.uk/svn/cmic/trunk/NiftyLink@8490 30ced1d1-d8fb-0310-b896-84269ee58dd6

--- Code/Libraries/Common/NiftyLinkUtils.h
+++ Code/Libraries/Common/NiftyLinkUtils.h
@@ -37,6 +37,8 @@
 #include "igtlObjectFactory.h"
 
 #include <cmath>
+#include <map>
+#include <string>
 #include "igtlMath.h"
 
 #include "NiftyLinkConfig.h"
@@ -52,7 +54,98 @@ static igtl::Matrix4x4 dummyTransformMatrix =
 {  0, 0.15, 0.98, 0},
 { 0, 0.98, -0.15, 50},
 { 0, 0, 0, 1} };
- 
+
+static enum MessageTypes 
+{
+  NONDEFINED,
+  BIND,
+  GET_BIND,
+  STT_BIND,
+  STP_BIND,
+  RTS_BIND,
+  COLORTABLE,
+  GET_COLORT,
+  STT_COLORT,
+  STP_COLORT,
+  RTS_COLORT,
+  IMAGE,
+  GET_IMAGE,
+  STT_IMAGE,
+  STP_IMAGE,
+  RTS_IMAGE,
+  IMGMETA,
+  GET_IMGMETA,
+  STT_IMGMETA,
+  STP_IMGMETA,
+  RTS_IMGMETA,
+  LBMETA,
+  GET_LBMETA,
+  STT_LBMETA,
+  STP_LBMETA,
+  RTS_LBMETA,
+  NDARRAY,
+  GET_NDARRAY,
+  STT_NDARRAY,
+  STP_NDARRAY,
+  RTS_NDARRAY,
+  POINTMSG,
+  GET_POINTMSG,
+  STT_POINTMSG,
+  STP_POINTMSG,
+  RTS_POINTMSG,
+  POLYDATA,
+  GET_POLYDATA,
+  STT_POLYDATA,
+  STP_POLYDATA,
+  RTS_POLYDATA,
+  POSITION,
+  GET_POSITION,
+  STT_POSITION,
+  STP_POSITION,
+  RTS_POSITION,
+  QTDATA,
+  GET_QTDATA,
+  STT_QTDATA,
+  STP_QTDATA,
+  RTS_QTDATA,
+  SENSOR,
+  GET_SENSOR,
+  STT_SENSOR,
+  STP_SENSOR,
+  RTS_SENSOR,
+  STATUS,
+  GET_STATUS,
+  STT_STATUS,
+  STP_STATUS,
+  RTS_STATUS,
+  STRING,
+  GET_STRING,
+  STT_STRING,
+  STP_STRING,
+  RTS_STRING,
+  TDATA,
+  GET_TDATA,
+  STT_TDATA,
+  STP_TDATA,
+  RTS_TDATA,
+  TRAJ,
+  GET_TRAJ,
+  STT_TRAJ,
+  STP_TRAJ,
+  RTS_TRAJ,
+  TRANSFORM,
+  GET_TRANS,
+  STT_TRANS,
+  STP_TRANS,
+  RTS_TRANS
+};
+
+// Map to associate the strings with the enum values
+typedef std::map<std::string, MessageTypes> mapStrMsgType;
+static mapStrMsgType strMsgTypes; 
+
+extern "C" NIFTYLINKCOMMON_WINEXPORT void InitMessageTypes(mapStrMsgType &types);
+
 /// \brief dummy function to test if the building and linking works correctly
 extern "C" NIFTYLINKCOMMON_WINEXPORT int NiftyLinkDummyFunction1();
 
