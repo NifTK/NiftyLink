d34be38afd7384f64622944a542b3ef4e56ad4a0
Gergely Zombori <zomboir@gmail.com>
2012-02-13 15:37:42 +0000

    #1303 - NiftyLink: Updated the OpenIGTLink version tag to the latest: 74a8daccb1
    git-svn-id: http://cmicdev.cs.ucl.ac.uk/svn/cmic/trunk/NiftyLink@8496 30ced1d1-d8fb-0310-b896-84269ee58dd6

--- CMake/CMakeExternals/OPENIGTLINK.cmake
+++ CMake/CMakeExternals/OPENIGTLINK.cmake
@@ -35,7 +35,7 @@ SET(OPENIGTLINK_DEPENDS ${proj})
 IF(NOT DEFINED OpenIGTLink_DIR)
 
     #1273 - OpenIGTLink version update due to bugfixes.
-    SET(revision_tag c34d51ec59)
+    SET(revision_tag 74a8daccb1)
     IF(${proj}_REVISION_TAG)
         SET(revision_tag ${${proj}_REVISION_TAG})
     ENDIF()
