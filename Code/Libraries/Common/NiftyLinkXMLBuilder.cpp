a5ea4d75c55d6e3d7f8eb048d57a20d270213b94
Gergely Zombori <zomboir@gmail.com>
2012-05-01 10:04:34 +0000

    #1393 - Moved XML descriptor creator to a separate class under NiftyLink/Libraries/Common. - Added command message support - Tested under mac and win
    git-svn-id: http://cmicdev.cs.ucl.ac.uk/svn/cmic/trunk/NiftyLink@8931 30ced1d1-d8fb-0310-b896-84269ee58dd6

---
+++ Code/Libraries/Common/NiftyLinkXMLBuilder.cpp
@@ -0,0 +1,268 @@
+/*=============================================================================
+
+ NiftyLink:  A software library to facilitate communication over OpenIGTLink.
+
+             http://cmic.cs.ucl.ac.uk/
+             http://www.ucl.ac.uk/
+
+ Copyright (c) UCL : See LICENSE.txt in the top level directory for details.
+
+ Last Changed      : $Date: 2010-05-25 17:02:50 +0100 (Tue, 25 May 2010) $
+ Revision          : $Revision: 3300 $
+ Last modified by  : $Author: mjc $
+
+ Original author   : m.clarkson@ucl.ac.uk
+
+ This software is distributed WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ PURPOSE.  See the above copyright notices for more information.
+
+ ============================================================================*/
+
+#include "NiftyLinkXMLBuilder.h"
+
+QString XMLBuilderBase::parseDescriptorType(QString xmlString)
+{
+  QDomDocument xmlDoco;
+
+  bool messageValid = xmlDoco.setContent(xmlString);
+
+  if (messageValid)
+  {
+     // A valid XML document was received, now it's time to parse it
+      QDomElement root = xmlDoco.documentElement();
+      
+      return root.tagName();
+  }
+  else return QString();
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+QString ClientDescriptorXMLBuilder::getXMLAsString(void)
+{
+  QDomDocument domDocument("ClientDescriptor");
+
+  QDomElement root = domDocument.createElement("ClientDescriptor");
+  
+  QDomElement device = domDocument.createElement("Device");
+  device.setAttribute("DeviceName", m_deviceName);
+  device.setAttribute("DeviceType", m_deviceType);
+  device.setAttribute("CommunicationType", m_commType);
+  device.setAttribute("PortName", m_portName);
+  root.appendChild(device);
+
+  QDomElement client = domDocument.createElement("Client");
+  device.setAttribute("ClientIP", m_clientIP);
+  device.setAttribute("ClientPort", m_clientPort);
+  root.appendChild(client);
+
+  domDocument.appendChild(root);
+
+  m_descriptorString.clear();
+  m_descriptorString.append(domDocument.toString());
+
+  QDomDocument xmlDoco;
+  m_messageValid = xmlDoco.setContent(m_descriptorString);
+
+  if (m_messageValid)
+    return m_descriptorString;
+  else
+    return QString();
+}
+
+void ClientDescriptorXMLBuilder::setXMLString(QString desc)
+{
+  m_descriptorString.clear();
+  m_descriptorString.append(desc);
+
+  QDomDocument xmlDoco;
+
+  m_messageValid = xmlDoco.setContent(desc);
+
+  if (m_messageValid)
+  {
+     // A valid XML document was received, now it's time to parse it
+      QDomElement root = xmlDoco.documentElement();
+      
+      if (root.tagName() == "ClientDescriptor")
+      {
+        QDomNode n = root.firstChild();
+        
+        while (!n.isNull())
+        {
+          QDomElement e = n.toElement();
+          
+          if (!e.isNull())
+          {
+            if (e.tagName() == "Device")
+            {
+              m_deviceName.clear();
+              m_deviceName.append(e.attribute("DeviceName", ""));
+              
+              m_deviceType.clear();
+              m_deviceType.append(e.attribute("DeviceType", ""));
+              
+              m_commType.clear();
+              m_commType.append(e.attribute("CommunicationType", ""));
+              
+              m_portName.clear();
+              m_portName.append("PortName: ");              
+            }
+            else if (e.tagName() == "Client")
+            {
+              m_clientIP.clear();
+              m_clientIP.append(e.attribute("ClientIP", ""));
+              
+              m_clientPort.clear();
+              m_clientPort.append(e.attribute("ClientPort", ""));
+            }
+          }
+
+          n = n.nextSibling();
+        }
+      }
+
+  }
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+void CommandDescriptorXMLBuilder::addParameter(QString pName, QString pType, QString pVal) 
+{ 
+  m_parameterNames.append(pName); 
+  m_parameterTypes.append(pType); 
+  m_parameterValues.append(pVal); 
+  m_numOfParameters++; 
+}
+
+QString CommandDescriptorXMLBuilder::getXMLAsString(void)
+{
+  QDomDocument domDocument("CommandDescriptor");
+
+  QDomElement root = domDocument.createElement("CommandDescriptor");
+  
+  QDomElement cmd = domDocument.createElement("Command");
+  cmd.setAttribute("CommandName", m_commandName);
+  cmd.setAttribute("NumOfParameters", m_numOfParameters);
+  
+  root.appendChild(cmd);
+
+  for (int i = 0; i < m_numOfParameters; i++)
+  {
+    QDomElement param = domDocument.createElement("Parameter");
+    param.setAttribute("Name", m_parameterNames.at(i));
+    param.setAttribute("Type", m_parameterTypes.at(i));
+    param.setAttribute("Value", m_parameterValues.at(i));
+    root.appendChild(param);
+  }
+
+  domDocument.appendChild(root);
+
+  m_descriptorString.clear();
+  m_descriptorString.append(domDocument.toString());
+
+  QDomDocument xmlDoco;
+  m_messageValid = xmlDoco.setContent(m_descriptorString);
+
+  if (m_messageValid)
+    return m_descriptorString;
+  else
+    return QString();
+}
+
+void CommandDescriptorXMLBuilder::setXMLString(QString desc)
+{
+  m_descriptorString.clear();
+  m_descriptorString.append(desc);
+
+  QDomDocument xmlDoco;
+
+  m_messageValid = xmlDoco.setContent(desc);
+
+  if (m_messageValid)
+  {
+     // A valid XML document was received, now it's time to parse it
+      QDomElement root = xmlDoco.documentElement();
+      
+      if (root.tagName() == "CommandDescriptor")
+      {
+        QDomNode n = root.firstChild();
+        
+        while (!n.isNull())
+        {
+          QDomElement e = n.toElement();
+          
+          if (!e.isNull())
+          {
+            if (e.tagName() == "Command")
+            {
+              m_commandName.clear();
+              m_commandName.append(e.attribute("CommandName", ""));
+              
+              bool ok = false;
+              m_numOfParameters = e.attribute("NumOfParameters", "").toInt(&ok, 10);       
+            }
+            else if (e.tagName() == "Parameter")
+            {
+              m_parameterNames.append(e.attribute("Name", ""));
+              m_parameterTypes.append(e.attribute("Type", ""));
+              m_parameterValues.append(e.attribute("Value", ""));
+            }
+          }
+
+          n = n.nextSibling();
+        }
+      }
+
+  }
+}
+
+QString CommandDescriptorXMLBuilder::getParameterName(int i)
+{
+  if (i >= m_parameterNames.count())
+    return QString();
+  
+  QString pname = m_parameterNames.at(i);
+  if (pname.isNull() || pname.isEmpty())
+    return QString();
+  else return pname;
+}
+
+QString CommandDescriptorXMLBuilder::getParameterType(int i)
+{
+  if (i >= m_parameterTypes.count())
+    return QString();
+  
+  QString ptype = m_parameterTypes.at(i);
+  if (ptype.isNull() || ptype.isEmpty())
+    return QString();
+  else return ptype;
+}
+
+QString CommandDescriptorXMLBuilder::getParameterValue(int i)
+{
+  if (i >= m_parameterValues.count())
+    return QString();
+  
+  QString pval = m_parameterValues.at(i);
+  if (pval.isNull() || pval.isEmpty())
+    return QString();
+  else return pval;
+}
+  
+QStringList CommandDescriptorXMLBuilder::getParameterNames(void)
+{
+  return m_parameterNames;
+}
+
+QStringList CommandDescriptorXMLBuilder::getParameterTypes(void)
+{
+  return m_parameterTypes;
+}
+
+QStringList CommandDescriptorXMLBuilder::getParameterValues(void)
+{
+  return m_parameterValues;
+}
---
+++ Code/Libraries/Common/NiftyLinkXMLBuilder.h
@@ -0,0 +1,145 @@
+/*=============================================================================
+
+ NiftyLink:  A software library to facilitate communication over OpenIGTLink.
+
+             http://cmic.cs.ucl.ac.uk/
+             http://www.ucl.ac.uk/
+
+ Copyright (c) UCL : See LICENSE.txt in the top level directory for details.
+
+ Last Changed      : $Date: 2010-05-25 17:02:50 +0100 (Tue, 25 May 2010) $
+ Revision          : $Revision: 3300 $
+ Last modified by  : $Author: mjc $
+
+ Original author   : m.clarkson@ucl.ac.uk
+
+ This software is distributed WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ PURPOSE.  See the above copyright notices for more information.
+
+ ============================================================================*/
+
+#ifndef NIFTYLINKXMLBUILDER_H
+#define NIFTYLINKXMLBUILDER_H
+
+#include <QString>
+#include <QUrl>
+#include <QHostInfo>
+#include <QHostAddress>
+#include <QStringList>
+#include <QNetworkConfigurationManager>
+#include <QNetworkInterface>
+#include <QNetworkSession>
+#include <QDomDocument>
+
+#include "NiftyLinkConfig.h"
+#include "NiftyLinkCommonWin32ExportHeader.h"
+
+#include "QsLog.h"
+#include "QsLogDest.h"
+
+class NIFTYLINKCOMMON_WINEXPORT XMLBuilderBase : public QObject
+{
+	Q_OBJECT
+
+public:
+  XMLBuilderBase() {}
+  virtual ~XMLBuilderBase() {}
+  
+  /// \brief Returns true if the message is valid
+	bool isMessageValid() { return m_messageValid; }
+
+  virtual QString getXMLAsString(void) { return QString(); }
+  virtual void setXMLString(QString desc) {}
+
+  virtual QString getDescriptorType(void) { return m_descriptorString; }
+  
+  static QString parseDescriptorType(QString xmlDoco);
+
+protected:
+  QString m_descriptorString;
+  bool m_messageValid;
+
+};
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+class NIFTYLINKCOMMON_WINEXPORT ClientDescriptorXMLBuilder : public XMLBuilderBase
+{
+	Q_OBJECT
+
+public:
+  /// \brief Basic constructor
+  ClientDescriptorXMLBuilder() : XMLBuilderBase() { m_messageValid = false; }
+  /// \brief Basic destructor
+	virtual ~ClientDescriptorXMLBuilder() {}
+
+  QString getXMLAsString(void);
+  void setXMLString(QString desc);
+
+  inline void setDeviceName(QString name) { m_deviceName.clear(); m_deviceName.append(name); }
+  inline QString getDeviceName(void) { return m_deviceName; }
+
+  inline void setDeviceType(QString type) { m_deviceType.clear(); m_deviceType.append(type); }
+  inline QString getDeviceType(void) { return m_deviceType; }
+
+  inline void setCommunicationType(QString commtype) { m_commType.clear(); m_commType.append(commtype); }
+  inline QString getCommunicationType(void) { return m_commType; }
+
+  inline void setPortName(QString portName) { m_portName.clear(); m_portName.append(portName); }
+  inline QString getPortName(void) { return m_portName; }
+
+  inline void setClientIP(QString ip) { m_clientIP.clear(); m_clientIP.append(ip); }
+  inline QString getClientIP(void) { return m_clientIP; }
+
+  inline void setClientPort(QString port) { m_clientPort.clear(); m_clientPort.append(port); }
+  inline QString getClientPort(void) { return m_clientPort;}
+
+private:
+  QString m_deviceName;
+  QString m_deviceType;
+  QString m_commType;
+  QString m_portName;
+  QString m_clientIP;
+  QString m_clientPort;
+};
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+class NIFTYLINKCOMMON_WINEXPORT CommandDescriptorXMLBuilder : public XMLBuilderBase
+{
+	Q_OBJECT
+
+public:
+  /// \brief Basic constructor
+  CommandDescriptorXMLBuilder() : XMLBuilderBase() { m_messageValid = false; m_numOfParameters = 0; }
+  /// \brief Basic destructor
+	virtual ~CommandDescriptorXMLBuilder() {}
+
+  QString getXMLAsString(void);
+  void setXMLString(QString desc);
+
+  inline void setCommandName(QString name) { m_commandName.clear(); m_commandName.append(name); }
+  inline QString getCommandName(void) { return m_commandName; }
+
+  void addParameter(QString pName, QString pType, QString pVal);
+  inline int  getNumOfParameters(void) { return m_numOfParameters; }
+  
+  QString getParameterName(int i);
+  QString getParameterType(int i);
+  QString getParameterValue(int i);
+  
+  QStringList getParameterNames(void);
+  QStringList getParameterTypes(void);
+  QStringList getParameterValues(void);
+
+private:
+  QString     m_commandName;
+  int         m_numOfParameters;
+  
+  QStringList m_parameterNames;
+  QStringList m_parameterTypes;
+  QStringList m_parameterValues;
+};
+
+#endif
