63e17aedd4281b46818c3e7d0ac81c1f784585d8
Gergely Zombori <zomboir@gmail.com>
2012-05-30 17:30:24 +0000

    #1492 - Changed the OIGTLink version tag to 7812256420
    git-svn-id: http://cmicdev.cs.ucl.ac.uk/svn/cmic/trunk/NiftyLink@9076 30ced1d1-d8fb-0310-b896-84269ee58dd6

--- CMake/CMakeExternals/OPENIGTLINK.cmake
+++ CMake/CMakeExternals/OPENIGTLINK.cmake
@@ -34,7 +34,7 @@ SET(OPENIGTLINK_DEPENDS ${proj})
  
 IF(NOT DEFINED OpenIGTLink_DIR)
 
-    SET(revision_tag e368df776c)
+    SET(revision_tag 7812256420)
     IF(${proj}_REVISION_TAG)
         SET(revision_tag ${${proj}_REVISION_TAG})
     ENDIF()
