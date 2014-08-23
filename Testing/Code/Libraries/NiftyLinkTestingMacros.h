d150a201ccd8c2111955ddf713f056e318ea38e6
Matt Clarkson <m.clarkson@ucl.ac.uk>
2014-08-23 14:15:10 +0100

    Bug #3609: Add NiftyLinkTestingMacros.h

---
+++ Testing/Code/Libraries/NiftyLinkTestingMacros.h
@@ -0,0 +1,39 @@
+/*=============================================================================
+  NiftyLink:  A software library to facilitate communication over OpenIGTLink.
+
+  Copyright (c) University College London (UCL). All rights reserved.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.
+
+  See LICENSE.txt in the top level directory for details.
+=============================================================================*/
+#ifndef NiftyLinkTestingMacros_h
+#define NiftyLinkTestingMacros_h
+
+#include <QObject>
+#include <QtTest/QtTest>
+#include <QCoreApplication>
+#include <QsLog.h>
+#include <QsLogDest.h>
+
+namespace niftk
+{
+
+#define NIFTYLINK_QTEST_MAIN(TestObject) \
+int main(int argc, char *argv[]) \
+{ \
+  QCoreApplication app(argc, argv); \
+  QsLogging::Logger& logger = QsLogging::Logger::instance(); \
+  logger.setLoggingLevel(QsLogging::DebugLevel); \
+  QsLogging::DestinationPtr debugDestination(QsLogging::DestinationFactory::MakeDebugOutputDestination() ); \
+  logger.addDestination(debugDestination.get()); \
+  QTEST_DISABLE_KEYPAD_NAVIGATION \
+  TestObject tc; \
+  return QTest::qExec(&tc, argc, argv); \
+}
+
+} // end namespace
+
+#endif // NiftyLinkTestingMacros_h
